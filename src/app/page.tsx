be83be79b7a92bfd99fceb7432bd769417ecab7d
Chase Lonas <chaselonas@gmail.com>
2024-04-30 18:18:03 -0400

    add family search logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,23 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-full border-b dark:border-neutral-800">
-          <p>A Family Search Clone. Clearly.</p>
+      <div className="w-full flex flex-row border-b dark:border-neutral-800">
+        <a
+          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/familysearch-tree-header.svg"
+            alt="Family Search Logo"
+            className="dark:invert"
+            width={132}
+            height={33.8}
+            priority
+          />
+        </a>
+        <p>A Family Search Clone. Clearly.</p>
       </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
